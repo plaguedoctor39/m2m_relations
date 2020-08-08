9508e5b38754700e7c1c67842148dc77bf8d50d6
daniil <shishovdanil@gmail.com>
2020-08-08 17:33:00 +0300

    fixed themes

--- articles/views.py
+++ articles/views.py
@@ -7,10 +7,12 @@ from articles.models import Article, ArticleScope
 def articles_list(request):
     template = 'articles/news.html'
     ordering = '-published_at'
+    scopes = list(ArticleScope.objects.all())
     articles = list(Article.objects.order_by(ordering))
     context = {'object_list': articles,
+               'scopes': scopes
                }
-
+    print(scopes)
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
 
